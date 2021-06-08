05c4d129fda09c1728c49ba95242caabcc25b696
Svissy <61078733+Svissy@users.noreply.github.com>
2021-06-08 11:24:26 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const menuElem = document.querySelector('.menu');
+const humburgerElem = document.querySelector('.humburger-menu');
+
+const toggleMenu = () => {
+  menuElem.classList.toggle('menu-active');
+  humburgerElem.classList.toggle('humburger-menu-active');
+};
+
+const closeMenu = () => {
+  menuElem.classList.remove('menu-active');
+  humburgerElem.classList.remove('humburger-menu-active');
+};
+
+humburgerElem.addEventListener('click', () => {
+  toggleMenu();
+});
+
+menuElem.addEventListener('click', event => {
+  if (event.target.classList.contains('menu-list__link')) {
+    closeMenu();
+  }
+});
